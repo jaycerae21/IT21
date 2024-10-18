bb503b1372eba766be2c653cf22122eb403e0893
Jayce Rae <20231641@nbsc.edu.ph>
2024-10-18 10:49:31 +0800

    Update linklist.java

--- AYSONCENIA/linklist.java
+++ AYSONCENIA/linklist.java
@@ -1,13 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.LinkedList;
+public class LINKEDLIST1 {
+    public static void main(String[] args) {
+        LinkedList<String> animals = new LinkedList<>();
 
-/**
- *
- * @author ITLAB2-PC12-STUDENT
- */
-public class linklist {
-    
+// insertion
+        animals.add("Dog");
+        animals.addFirst("Cat");
+        animals.addLast("Elephant");
+        animals.add(1, "Lion");
+
+        System.out.println("Linked List Original: " + animals);
+        System.out.println(animals.size());
+
+//  Seraching  
+        if (animals.contains("Lion")) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+        boolean containslion = animals.contains("Lion");
+        System.out.println(containslion);
+
+//        Deletion
+//        animals.remove();
+//        animals.removeFirst();
+//        animals.removeLast();
+//        animals.remove(2);
+//        System.out.println("LinkedList After Deletion: " + animals);
+//        Insertion:    add(); addFirst();  addLast();  add(index, data);
+//         Deletion:    remove();   removeFirst();  removeLast(); remove(index);
+//          Searching operation:        contains(element); size();
+    }
+
 }
+
